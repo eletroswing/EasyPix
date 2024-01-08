f6302e5363f16312d2ab1c9410df50027eb5c40c
eletroswing <ytalosilvabatalha@gmail.com>
2024-01-08 17:31:51 -0300

    build(jest config): adding jest config to run the tests in typescript

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-    "fakeTimers": {"enableGlobally": true}
-  };
+  fakeTimers: { enableGlobally: true },
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  testRegex: "(/tests/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+};
